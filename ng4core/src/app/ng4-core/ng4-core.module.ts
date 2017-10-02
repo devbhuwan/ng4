925cf52347b962f626f97e1afd67ab88a1ef2313
devbhuwan <bhuwan.upadhyay49@gmail.com>
2017-10-02 15:34:57 +0400

    ng4

--- ng4core/src/app/ng4-core/ng4-core.module.ts
+++ ng4core/src/app/ng4-core/ng4-core.module.ts
@@ -2,13 +2,28 @@ import {ModuleWithProviders, NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {Ng4CoreComponent} from './ng4-core.component';
 import {NG4CORE_CONFIG, Ng4CoreConfig} from "./config";
+import {TranslateHttpLoader} from "@ngx-translate/http-loader";
+import {HttpClient} from "@angular/common/http";
+import {TranslateLoader, TranslateModule} from "@ngx-translate/core";
+import {TranslateStore} from "@ngx-translate/core/src/translate.store";
+import {HttpClientModule} from "@angular/common/http";
+
+
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, './../i18n/', '.json');
+}
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    HttpClientModule,
+    TranslateModule.forChild({
+      loader: {provide: TranslateLoader, useFactory: createTranslateLoader, deps: [HttpClient]},
+      isolate: true
+    })
   ],
   declarations: [Ng4CoreComponent],
-  exports: [Ng4CoreComponent]
+  exports: [Ng4CoreComponent],
 })
 export class Ng4CoreModule {
 
@@ -17,7 +32,8 @@ export class Ng4CoreModule {
     return {
       ngModule: Ng4CoreModule,
       providers: [
-        {provide: NG4CORE_CONFIG, useValue: config}
+        {provide: NG4CORE_CONFIG, useValue: config},
+        TranslateStore, HttpClient
       ],
     };
   }
