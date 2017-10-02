09171636863cbe5d05b4f15da82e40b5c9154a62
devbhuwan <bhuwan.upadhyay49@gmail.com>
2017-10-02 13:36:36 +0400

    ng4

--- ng4core/src/app/ng4-core/ng4-core.module.ts
+++ ng4core/src/app/ng4-core/ng4-core.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Ng4CoreComponent } from './ng4-core.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [Ng4CoreComponent],
+  exports: [Ng4CoreComponent]
 })
 export class Ng4CoreModule { }
