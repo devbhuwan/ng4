506bc3ed69ef69fb7635505e639504411c448ecd
devbhuwan <bhuwan.upadhyay49@gmail.com>
2017-10-03 08:18:57 +0400

    translate

--- ng4-bpm/src/app/ng4-bpm.module.ts
+++ ng4-bpm/src/app/ng4-bpm.module.ts
@@ -2,7 +2,7 @@ import {ModuleWithProviders, NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {Ng4BpmConfig} from "./ng4-bpm/config";
 import {Ng4BpmComponent} from './ng4-bpm/ng4-bpm.component';
-import {TranslateModule} from "@ngx-translate/core";
+import {TranslateModule, TranslateService} from "@ngx-translate/core";
 import { Ng4CoreModule } from "ng4-core";
 
 @NgModule({
@@ -16,6 +16,11 @@ import { Ng4CoreModule } from "ng4-core";
 })
 export class Ng4BpmModule {
 
+
+  constructor(private translateService: TranslateService) {
+    let lang = this.translateService.getDefaultLang();
+    this.translateService.setDefaultLang("ar");
+  }
   /* optional: in case you need users to override your providers */
   static forRoot(config?: Ng4BpmConfig): ModuleWithProviders {
     return {
